84f87901392b3918bea1b01df93a72e6651bf53b
Julián Grajales <61485178+fredoomsearch@users.noreply.github.com>
2025-06-21 01:46:56 -0500

    Update event_service.py

--- services/event_service.py
+++ services/event_service.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
-from Backend.repositories import event_repo
-from Backend.api.events import EventCreate
+from repositories import event_repo
+from api.events import EventCreate
 
 def create_event(event: EventCreate, db: Session):
     return event_repo.create_event(db, event)
