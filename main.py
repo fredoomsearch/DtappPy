2097dde6881a96b044197a3abdf04ed71e591b29
Julián Grajales <61485178+fredoomsearch@users.noreply.github.com>
2025-06-21 02:52:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import hashlib
 from fastapi import FastAPI, HTTPException, Request, logger
 from fastapi.responses import HTMLResponse
-from api import crypto, events, data_processing, ml_models
+from api import crypto, data_processing, ml_models
 from utils import database
 from models import crypto_model, event_model, scraped_data_model
 from fastapi.middleware.cors import CORSMiddleware  # Import CORS
